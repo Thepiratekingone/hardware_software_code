085de152425db07bcac6e2f6a99f1db475ca9739
Thepiratekingone <amares.spence@parkeasths.org>
2025-01-16 13:08:18 -0500

    amares spence_project_01.py

---
+++ amares spence_project_01.py
@@ -0,0 +1,17 @@
+def main():
+    print("we want to know the date and time")
+    print()
+    print("what is today date {}?".format(name))
+    answer = input()
+    print("what is the time {}?".format(name))
+    answer = input()
+
+def main():
+        greeting(input("What is your name?"))
+        print("What college are you attending {}?".Format(name))
+        answer = input()
+        print("what high school you adttend {}?".Format(name))
+        answer = input()
+
+if __name__ == "__main__":
+    main()
