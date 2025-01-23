a6007437e5f53ca589af80a768a74b30c6ea7d24
Thepiratekingone <amares.spence@parkeasths.org>
2025-01-23 13:11:02 -0500

    git hub

---
+++ Amares spence_project_02.py
@@ -0,0 +1,21 @@
+def main():
+    print("we want to know all about you")
+
+def repat_qustions(count = 1):
+    print_qustion()
+    print_qustion(2)
+
+def main():
+    repat_qustions()
+
+def conversation():
+    print("Do you watch anime?")
+    answer = input().lower().strip()
+
+def conversation():
+    print("which video games do you like?")
+    answer = input().lower().strip()
+
+if __name__ == "__main__":
+    main()
+    
