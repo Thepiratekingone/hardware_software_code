f0e1c31abe7b1ad241d20a607fbaae5c14080ac8
Thepiratekingone <amares.spence@parkeasths.org>
2025-01-28 13:20:39 -0500

    gitHub

---
+++ amares_spence_project_01.py
@@ -0,0 +1,15 @@
+def main():
+    print("hello my name is ms.spence. I'm going to ask you qustions.")
+    username = input("what is your name?:")
+    print("Username is: " + username)
+    college = input("what college do you go to?:")
+    print("college is:" + college)
+    Highschool = input("what high school did you go to?:")
+    print("Highschool is:" + Highschool)
+    print("which one was more  fun highschool or college?")
+    last_qustion = input("which one was more fun highschool or college?:")
+    print("last_qustion is:" + last_qustion)
+    print("thank you you for using this program ms.spence i hope you use it again!!!")
+
+if __name__ == "__main__":
+    main()
